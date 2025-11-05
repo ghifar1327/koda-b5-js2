a5191229f022745860d6d7e3a673070bd241246d
ghifar1327 <givartorreto@gmail.com>
2025-11-05 21:47:19 +0700

    Add files via upload

--- ifElse.js
+++ ifElse.js
@@ -1,12 +1,16 @@
-let subtotal = 1.5e6
-if (subtotal>0){
-    if (subtotal>= 5e5){
-        console.log(subtotal-(subtotal * 5/100))
-    }else if (subtotal > 1e6){
-        console.log(subtotal-(subtotal * 10/100))
-    }else {
-        console.log(subtotal)
-    }
-}else{
-    console.log("subtotal: invalid");
+let subtotal = 5e5;
+if (subtotal > 0) {
+  if (subtotal > 1e6) {
+    console.log(
+      `subtotal: Rp.${subtotal - (subtotal * 10) / 100} kamu dapat diskon 10%`
+    );
+  } else if (subtotal >= 5e5) {
+    console.log(
+      `subtotal: Rp.${subtotal - (subtotal * 5) / 100} kamu dapat diskon 5%`
+    );
+  } else {
+    console.log(`subtotal: Rp.${subtotal}`);
+  }
+} else {
+  console.log("subtotal: invalid");
 }
--- switchCase.js
+++ switchCase.js
@@ -1,20 +1,20 @@
-let subtotal = 10;
+let subtotal = -1e6;
 
 switch (true) {
-  case subtotal > 0:
-    switch (true) {
-      case subtotal >= 5e5:
-        console.log(subtotal - (subtotal * 5) / 100);
-        break;
-      case subtotal > 1e6:
-        console.log(subtotal - (subtotal * 5) / 100);
-        break;
-      default:
-        console.log(subtotal);
-        break;
-    }
+  case subtotal < 0:
+    console.log("subtotal: invalid");
+    break;
+  case subtotal >= 5e5:
+    console.log(
+      `subtotal: Rp.${subtotal - (subtotal * 5) / 100}, kamu dapat diskon 5%`
+    );
+    break;
+  case subtotal > 1e6:
+    console.log(
+      `subtotal: Rp.${subtotal - (subtotal * 10) / 100} kamu dapat diskon 10%`
+    );
     break;
   default:
-    console.log("subtotal: invalid");
+    console.log(`subtotal: Rp.${subtotal}`);
     break;
 }
